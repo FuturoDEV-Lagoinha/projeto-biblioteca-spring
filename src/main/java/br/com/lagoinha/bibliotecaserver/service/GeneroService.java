6719f610c90cbd1fe333395128481bef51f5108b
ViniAmaralAndrade <vini.amaralgfa@gmail.com>
2023-03-29 21:48:58 -0300

    genero criado

---
+++ src/main/java/br/com/lagoinha/bibliotecaserver/service/GeneroService.java
@@ -0,0 +1,35 @@
+package br.com.lagoinha.bibliotecaserver.service;
+
+import br.com.lagoinha.bibliotecaserver.entity.Genero;
+import br.com.lagoinha.bibliotecaserver.repository.GeneroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GeneroService {
+
+    @Autowired
+private GeneroRepository generoRepository;
+
+    public List<Genero> listar() {
+        return this.generoRepository.findAll();
+    }
+
+    public Genero salvar(Genero genero){
+        return this.generoRepository.save(genero);
+    }
+
+    public Genero atualizar(Long id, Genero generoAtualizado) {
+        Optional<Genero> generoPesquisado = this.generoRepository.findById(id);
+        if(generoPesquisado.isPresent()) {
+            generoPesquisado.get().setNome(generoAtualizado.getNome());
+            return this.generoRepository.save(generoPesquisado.get());
+        }
+        return null;
+    }
+
+
+}
